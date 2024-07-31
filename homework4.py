5da35bdf89105063543a016e1b015c5f60ba5b30
Sumka111 <dima.kanaev.02@mail.ru>
2024-07-31 22:14:34 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string = input('Введённый текст: ')
+#print(len(my_string))
+print(my_string.upper())
+print(my_string.upper().lower())
+print(my_string.replace( ' ' , ''))
+print(my_string [1])
+print(my_string [-1])
